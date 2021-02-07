a70acafd76b0c1ba61aebeb040c889e3f0a09b9b
a-shevelev <60062720+a-shevelev@users.noreply.github.com>
2021-02-07 13:14:32 +0300

    add ws server

---
+++ serverWS/script.js
@@ -0,0 +1,16 @@
+let wss = require("ws").Server;
+
+let server = new wss({port: 8081});
+let clients = new Set();
+
+server.on("connection", function(socket) {
+	clients.add(socket);
+	socket.on("message", function(message){
+		for(let client of clients){
+			client.send(message);
+		}
+	});
+	socket.on("close", function(){
+		clients.delete(socket);
+	});
+});
